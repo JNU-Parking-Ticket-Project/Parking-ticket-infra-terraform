9fc71f692db3dadafb8f91501ee60f83d7988456
Kwon770 <sckwon770@gmail.com>
2024-09-05 00:09:43 +0900

    feat: Add connection with terraform cloud

--- jnu-parking-prod-was/idle/main.tf
+++ jnu-parking-prod-was/idle/main.tf
@@ -1,10 +1,18 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    cloud { 
+        organization = "sckwon770" 
+
+        workspaces { 
+        name = "jnu-parking" 
+        } 
+    } 
+    
+    required_providers {
+        aws = {
+        source  = "hashicorp/aws"
+        version = "~> 5.0"
+        }
     }
-  }
 }
 
 provider "aws" {
